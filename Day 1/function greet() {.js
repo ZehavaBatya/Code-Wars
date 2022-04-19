04f9a1c0c6471c16a07b3bff8e9a3a9346718e74
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-04-18 21:58:49 -0400

    Add files via upload

---
+++ Day 1/function greet() {.js
@@ -0,0 +1,11 @@
+function greet() {
+    return "hello world!";
+    }
+    
+multiply = function (a, b) {
+    return a * b;
+    }
+    
+function removeChar(str) {
+    return str.slice(1, -1);
+    }
