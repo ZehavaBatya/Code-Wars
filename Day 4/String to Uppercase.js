4c70d55e0fc068315c868dc3d8c0f5b26229b4a4
Annaliese B <84485729+ZehavaBatya@users.noreply.github.com>
2022-04-20 22:00:23 -0400

    Add files via upload

---
+++ Day 4/String to Uppercase.js
@@ -0,0 +1,12 @@
+//Write a function which converts the input string to uppercase.
+function makeUpperCase(str) {
+    let makeUpperCase = str.toUpperCase();
+    return makeUpperCase;
+  }
+  ("hello world");
+
+//  Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
